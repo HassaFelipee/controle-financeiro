28881338513c06e962a3a75a612a5e1e0c5ce91a
ericaafernanda <ericaferoliveira265@gmail.com>
2024-10-24 22:28:36 -0300

    testeCommit

--- src/main/java/com/fatec/controle_financeiro/domain/contasreceber/ContasReceberRepository.java
+++ src/main/java/com/fatec/controle_financeiro/domain/contasreceber/ContasReceberRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fatec.controle_financeiro.entities.ContasReceber;
 
 public interface ContasReceberRepository extends JpaRepository<ContasReceber, Long> {
+    //TesteCommit
     
 }
